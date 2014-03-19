97e1b4e7f7980786d37b50d327b4d67964223f48
Torben <mail@tknerr.de>
2014-03-19 17:44:11 +0100

    add lxc-vagrant-wrappers to avoid sudo password prompts

--- recipes/lxc.rb
+++ recipes/lxc.rb
@@ -10,3 +10,28 @@ bash "sudo aa-complain /usr/bin/lxc-start"
 # XXX: should be installed per-project via bindler!
 install_vagrant_plugin "vagrant-lxc", "0.5.0"
 
+# TODO:
+# * set VAGRANT_DEFAULT_PROVIDER=lxc
+# * set alternate OMNIBUS_INSTALL_URL
+
+# see https://github.com/fgrehm/vagrant-lxc/wiki/Avoiding-'sudo'-passwords
+file "/usr/bin/lxc-vagrant-wrapper" do
+  action :create
+  owner "root"
+  group "root"
+  mode 00755
+  content <<-EOH
+#!/usr/bin/env ruby
+exec ARGV.join(' ')
+EOH
+end
+file "/etc/sudoers.d/vagrant-lxc" do
+  action :create
+  owner "root"
+  group "root"
+  mode 00644
+  content <<-EOH
+#{node['devbox']['user']} ALL=NOPASSWD:/usr/bin/lxc-vagrant-wrapper
+EOH
+end
+
