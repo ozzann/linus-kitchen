2c6c7cdb22af3a799d024133ddf4c82290fbba8a
Torben Knerr <mail@tknerr.de>
2015-12-24 00:59:11 +0100

    ensure the build-essential package is installed (which is not present in our docker basebox)

--- cookbooks/vm/recipes/base.rb
+++ cookbooks/vm/recipes/base.rb
@@ -8,6 +8,6 @@ end
 
 include_recipe 'apt'
 
-%w( vim git libxml2-dev libxslt1-dev zlib1g-dev liblzma-dev ).each do |pkg|
+%w( vim git build-essential libxml2-dev libxslt1-dev zlib1g-dev liblzma-dev ).each do |pkg|
   package pkg
 end
